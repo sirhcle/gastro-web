6d0c673ff88f266b0f5e1483a7adadb50cb8a9f2
sirhcle <sirhcle@gmail.com>
2020-05-30 14:20:31 -0500

    vistas faltantes
    vistas faltantes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from '../modals/login/login.component';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,7 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   userData: any;
-  constructor(private modalService: NgbModal, private router: Router ) {
+  constructor(private modalService: NgbModal, private router: Router, private spinner: NgxSpinnerService ) {
     const locStorage = localStorage.getItem('userData');
     this.userData = JSON.parse(locStorage);
   }
@@ -25,6 +26,22 @@ export class NavbarComponent implements OnInit {
 
   openMiPlan() {
     // this.router.navigate(['/mi-plan']);
+  }
+
+  closeSesssion() {
+    localStorage.removeItem('userData');
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+      this.router.navigate(['/home']);
+      // window.location.reload();
+    }, 5000);
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
   }
 
   openSuscripcion() {
@@ -41,6 +58,10 @@ export class NavbarComponent implements OnInit {
 
   openClasesGratis() {
     this.router.navigate(['/clases-gratis']);
+  }
+
+  openStore() {
+    this.router.navigate(['/store']);
   }
 
 }
