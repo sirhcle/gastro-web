307f8c264b5bd86c7ba284eef0d61f47595995c8
sirhcle <sirhcle@gmail.com>
2020-06-21 18:51:37 -0500

    servicios
    se agregaron servicios para login
    se agregó una libreria para cambiar string a md5

--- src/app/components/modals/login/login.component.ts
+++ src/app/components/modals/login/login.component.ts
@@ -6,6 +6,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Router } from '@angular/router';
 
 import { RegisterComponent } from './../register/register.component';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +17,13 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private modalService: NgbModal, public activeModal: NgbActiveModal, private spinner: NgxSpinnerService, private router: Router) {
+  constructor(private modalService: NgbModal, public activeModal: NgbActiveModal, private spinner: NgxSpinnerService, private router: Router, private _loginService: LoginService) {
+
     this.loginForm = new FormGroup({
       username: new FormControl(),
       password: new FormControl()
     });
+
   }
 
   ngOnInit(): void {
@@ -35,23 +38,31 @@ export class LoginComponent implements OnInit {
     // console.log(this.loginForm);
     // console.log(this.loginForm.value);
     // console.log(this.loginForm.value.username);
-
     this.spinner.show();
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      
-      const obUsuario = {
-        username : this.loginForm.value.username,
-        password : this.loginForm.value.password,
-        isLogin : true
-      };
 
-      this.spinner.hide();
-      localStorage.setItem('userData', JSON.stringify(obUsuario) );
-      // window.location.reload();
-      this.router.navigate(['/cursos-online']);
-      this.activeModal.close();
-    }, 5000);
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+
+    this._loginService.doLogin(username, password)
+        .subscribe((resp: any) => {
+          console.log(resp);
+        });
+    
+    // setTimeout(() => {
+    //   /** spinner ends after 5 seconds */
+      
+    //   const obUsuario = {
+    //     username : this.loginForm.value.username,
+    //     password : this.loginForm.value.password,
+    //     isLogin : true
+    //   };
+
+    //   this.spinner.hide();
+    //   localStorage.setItem('userData', JSON.stringify(obUsuario) );
+    //   // window.location.reload();
+    //   this.router.navigate(['/cursos-online']);
+    //   this.activeModal.close();
+    // }, 5000);
   }
 
 }
