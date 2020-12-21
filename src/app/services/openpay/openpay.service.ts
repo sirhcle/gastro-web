9d097e5771f1bb7a9bc5f358a29c8bc197e326c4
sirhcle <sirhcle@gmail.com>
2020-12-20 20:36:00 -0600

    token open pay
    token open pay

--- src/app/services/openpay/openpay.service.ts
+++ src/app/services/openpay/openpay.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tokenName } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
@@ -20,19 +21,42 @@ export class OpenpayService {
       headers);
   }
 
-
-  openPayCreaSuscripcion(customerID: string, cardNumer: string, holderName: string, expiration: string, cvv: string, tipoPlan: string) {
-     
+  openPayCreaToken(cardNumer: string, holderName: string, expiration: string, cvv: string) {
     const expirationArr = expiration.split('/');
     const expirationYear = expirationArr[1];
     const expirationMonth = expirationArr[0];
 
-    //SANDBOX
-    //plan-id basico => pvtolvs4f9gdtyej6jj2     <- basica
-    //plan-id premium => pi1tmvt9ftp87avkpfwk    <- premium
-    //plan-id golden => pooxhdz1l72d0h5340er     <- golden
+    const raw = JSON.stringify({
+      card_number: cardNumer,
+      holder_name: holderName,
+      expiration_year: expirationYear,
+      expiration_month: expirationMonth,
+      cvv2: cvv
+    });
+
+    const headers = {
+      headers: new HttpHeaders()
+        .set('Authorization', 'Basic c2tfMDE4MDc2Mjg4NTJiNDIyZmE5ZmY2NDcyNDQzZGVkYTg6')
+        .set('Content-Type', 'application/json')
+    };
+    return this.httpClient.post(
+      'https://sandbox-api.openpay.mx/v1/mkhlhyhzipnwjnikydcr/tokens',
+      raw,
+      headers);
+  }
 
 
+  // tslint:disable-next-line:max-line-length
+  openPayCreaSuscripcion(customerID: string, cardNumer: string, holderName: string, expiration: string, cvv: string, tipoPlan: string, tokenID: string) {
+
+    const expirationArr = expiration.split('/');
+    const expirationYear = expirationArr[1];
+    const expirationMonth = expirationArr[0];
+
+    // SANDBOX
+    // plan-id basico => pvtolvs4f9gdtyej6jj2     <- basica
+    // plan-id premium => pi1tmvt9ftp87avkpfwk    <- premium
+    // plan-id golden => pooxhdz1l72d0h5340er     <- golden
 
     let planID = '';
 
@@ -47,15 +71,17 @@ export class OpenpayService {
     }
 
     const raw = JSON.stringify({
-      card: {
+      /*card: {
         card_number: cardNumer,
         holder_name: holderName,
         expiration_year: expirationYear,
         expiration_month: expirationMonth,
         cvv2: cvv,
         device_session_id: 'kR1MiQhz2otdIuUlQkbEyitIqVMiI16f'
-     },
-     plan_id: planID
+     },*/
+     source_id: tokenID,
+     plan_id: planID,
+     status: 'active'
     });
 
     const headers = {
@@ -68,6 +94,8 @@ export class OpenpayService {
       raw,
       headers);
   }
+
+  
 
   openPayCrearCliente(_name: string, _email:string){
     const raw = JSON.stringify({
